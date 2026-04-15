c737ea3973cbfc897a049f3c27f9c1e8c70868fb
Narendra Singh Kushwah <108320570+narendrasaanvi@users.noreply.github.com>
2026-04-15 19:08:18 +0530

    Update pay.blade.php

--- pay.blade.php
+++ pay.blade.php
@@ -1,16 +1,31 @@
-<h2>Complete Payment</h2>
-
-@if($iframe_src)
-
-<iframe
-    src="{{$iframe_src}}"
-    width="100%"
-    height="700"
-    frameborder="0">
-</iframe>
-
-@else
-
-Payment Error
-
-@endif
+@extends('layouts.frontend.master')
+@section('content')
+<style>
+    hr {
+        margin: 8px;
+    }
+</style>
+<x-toastr-notifications />
+<section class="wrapper bg-light mt-4">
+    <div class="container py-10">
+        <div class="row g-4">
+            <div class="col-lg-12">
+                <div class="card shadow-lg border-0">
+                    <div class="card-body p-4">
+                        @if($iframe_src)
+                        <iframe
+                            src="{{$iframe_src}}"
+                            width="100%"
+                            height="700"
+                            frameborder="0">
+                        </iframe>
+                        @else
+                        Payment Error
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
